718b921941e616e072a59a9d7388135ee2bce4bd
Kannan Muthu <kannan@arubanetworks.com>
2022-01-13 17:04:36 +0000

    build and start script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3 -m virtualenv virtualenv
+
+source virtualenv/bin/activate
+
+pip3 install -r requirements.txt
+
+
--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-pip3 virtualenv
-
-python3 -m virtualenv virtualenv
-
 source virtualenv/bin/activate
 
-pip3 install -r requirements.txt
-
-python app.py
+python app.py > /tmp/new_begingings.py
